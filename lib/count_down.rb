f1cd7d91fde7b3715d5c71aceb95c1c7d45ecfb0
Karina Guerra <kpguerra96@gmail.com>
2020-09-17 22:09:56 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,5 +3,10 @@ count_down = 10
 
 while (count_down > 0) do 
   puts count_down
-  (count_down == 1) ? "Happy New Year!" : count_down --
+  if (count_down == 1) 
+    puts "Happy New Year!" 
+    break 
+    else
+      count_down --
+    end 
 end 
