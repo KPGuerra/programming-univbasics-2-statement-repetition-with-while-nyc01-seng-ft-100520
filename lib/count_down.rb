735925334753cd0733009b705d51819f8c4ef43b
Karina Guerra <kpguerra96@gmail.com>
2020-09-17 22:10:53 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,12 +1,7 @@
 # Write your code here
 count_down = 10
 
-while (count_down > 0) do 
+while (count_down >= 1) do 
   puts count_down
-  if (count_down == 1) 
-    puts "Happy New Year!" 
-    break 
-    else
-      count_down --
-    end 
+  count_down --
 end 
